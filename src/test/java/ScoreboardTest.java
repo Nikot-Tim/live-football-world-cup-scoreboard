610544adb57f3d5c4bce13206e09f7d495612803
mikita-tsimoshchanka <nikita.timoshchenko123@gmail.com>
2025-02-07 14:11:52 +0100

    test: added validation failed test to prevent duplicate matches in startMatch()

--- src/test/java/ScoreboardTest.java
+++ src/test/java/ScoreboardTest.java
@@ -1,4 +1,5 @@
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import com.sportradar.test.lib.FootballMatch;
 import com.sportradar.test.lib.Scoreboard;
 import org.junit.jupiter.api.Test;
@@ -19,5 +20,17 @@ public class ScoreboardTest {
         assertEquals("Canada", matches.get(0).awayTeam());
         assertEquals(0, matches.get(0).scores().homeTeamScore());
         assertEquals(0, matches.get(0).scores().awayTeamScore());
+    }
+
+    @Test
+    void shouldNotAllowDuplicateMatches() {
+        Scoreboard scoreboard = new Scoreboard();
+
+        scoreboard.startMatch("Mexico", "Canada");
+
+        Exception exception = assertThrows(IllegalArgumentException.class, () ->
+            scoreboard.startMatch("Mexico", "Canada"));
+
+        assertEquals("Match already exists: Mexico vs Canada", exception.getMessage());
     }
 }
