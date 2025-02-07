c818276e4e7fca031d2eb6554cedbb0aa05ebfae
mikita-tsimoshchanka <nikita.timoshchenko123@gmail.com>
2025-02-07 18:00:08 +0100

    feat: added implementation for check the existence of a match in validator

--- src/main/java/com/sportradar/test/lib/validation/Validator.java
+++ src/main/java/com/sportradar/test/lib/validation/Validator.java
@@ -1,11 +1,15 @@
 package com.sportradar.test.lib.validation;
 
 import com.sportradar.test.lib.FootballMatch;
+import com.sportradar.test.lib.exception.MatchNotFoundException;
 import java.util.Map;
 
 public class Validator {
 
     public void validateMatchExists(String matchKey, Map<String, FootballMatch> matches) {
+        if (!matches.containsKey(matchKey)) {
+            throw new MatchNotFoundException(matchKey);
+        }
     }
 
 }
