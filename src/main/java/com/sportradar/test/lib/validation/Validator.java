f9ba2e914b8ed61811854bdcb0a42e6a3aaa5519
mikita-tsimoshchanka <nikita.timoshchenko123@gmail.com>
2025-02-07 18:09:07 +0100

    feat: added implementation for validator if scores are negative

--- src/main/java/com/sportradar/test/lib/validation/Validator.java
+++ src/main/java/com/sportradar/test/lib/validation/Validator.java
@@ -20,6 +20,9 @@ public class Validator {
     }
 
     public void validateScores(int homeScore, int awayScore) {
+        if (homeScore < 0 || awayScore < 0) {
+            throw new IllegalArgumentException("Scores cannot be negative.");
+        }
     }
 
 }
