1b4254534d411fd661ab6682b090986261e14741
mikita-tsimoshchanka <nikita.timoshchenko123@gmail.com>
2025-02-07 14:01:08 +0100

    feat: basic implementation startMatch() and getSummary()

--- src/main/java/com/sportradar/test/lib/Scoreboard.java
+++ src/main/java/com/sportradar/test/lib/Scoreboard.java
@@ -1,12 +1,16 @@
 package com.sportradar.test.lib;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Scoreboard {
+    private final List<FootballMatch> matches = new ArrayList<>();
+
     public void startMatch(String homeTeam, String awayTeam) {
+        matches.add(new FootballMatch(homeTeam, awayTeam, new MatchScores(0, 0)));
     }
 
     public List<FootballMatch> getSummary() {
-        return List.of();
+        return matches;
     }
 }
